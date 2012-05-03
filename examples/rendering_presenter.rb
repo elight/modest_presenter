9d1140dd92931c29b56a2a7a57117fd799475bcb
Evan Light <evan.light@tripledogdare.net>
2012-05-03 18:13:39 -0400

    Stupid Presenter example using plain ol' ERB (no ActionView stuff yet)

---
+++ examples/rendering_presenter.rb
@@ -0,0 +1,75 @@
+$LOAD_PATH << "../lib"
+
+require 'stupid_presenter'
+
+Person = Struct.new(:first_name, :last_name, :age)
+StupidPerson = Class.new(Person)
+Nerd = Class.new(Person)
+
+evan = Nerd.new("Evan", "Light", 38)
+ed = Nerd.new("Ed", "Kim", 27)
+
+# Seems a bit generous calling this guy a Person
+w = StupidPerson.new("George W.", "Bush", 66)
+
+class PersonPresenter < StupidPresenters::StupidPresenter
+  def name
+    "#{first_name} #{last_name}"
+  end
+
+  def title
+  end
+end
+
+class NerdPresenter < PersonPresenter
+  def name
+    "#{super} is a nerd."
+  end
+end
+
+class StupidPersonPresenter < PersonPresenter
+  def name
+    "#{super} wastes oxygen better used by nerds."
+  end
+
+  def title
+    "President "
+  end
+end
+
+def present(model, context, &block)
+  Object.const_get("#{model.class}Presenter")
+        .new(model, context)
+        .present(&block)
+end
+
+people = [evan, ed, w]
+
+ERB_TEMPLATE = <<-ERB
+  <ul>
+    <% people.each do |person| %>
+      <% present(person, self) do %>
+        <li>
+          <%= title %><%= name %>
+        </li>
+      <% end %>
+    <% end %>
+  </ul>
+  <p>Yes, only stupid people are President</p>
+ERB
+
+require 'erb'
+puts ERB.new(ERB_TEMPLATE).result(binding)
+
+# OUTPUTS:
+# <ul>
+#   <li>
+#     Evan Light is a nerd.
+#   </li>
+#   <li>
+#     Ed Kim is a nerd.
+#   </li>
+#   <li>
+#     President George W. Bush wastes oxygen better used by nerds.
+#   </li>
+# </ul>
